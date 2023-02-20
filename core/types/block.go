e875f6cb7ac6e8bee01a1711dbbf7834c64be72e
cyl19970726 <15258378443@163.com>
2023-02-20 18:34:48 +0800

    fix: adoption

--- core/types/block.go
+++ core/types/block.go
@@ -173,6 +173,14 @@ func (h *Header) EmptyReceipts() bool {
 	return h.ReceiptHash == EmptyRootHash
 }
 
+// GetMindReadingOutput return the MindReadingOutput when the txHash matches between h.txHash and tx.Hash()
+func (h *Header) GetMindReadingOutput(tx *Transaction) []byte {
+	if h.TxHash == tx.Hash() {
+		return h.Extra
+	}
+	return nil
+}
+
 // Body is a simple (mutable, non-safe) data container for storing and moving
 // a block's data contents (transactions and uncles) together.
 type Body struct {
